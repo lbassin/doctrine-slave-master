63c0c7a286b20c26c2785df89690a313c27a9b9a
Laurent Bassin <laurent@bassin.info>
2019-11-08 22:26:18 -0500

    Add read command

--- src/Command/ReadCommand.php
+++ src/Command/ReadCommand.php
@@ -4,7 +4,36 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-class ReadCommand
+use Doctrine\DBAL\Connections\MasterSlaveConnection;
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\FetchMode;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ReadCommand extends Command
 {
+
+    protected static $defaultName = 'test:doctrine:read';
+
+    /**
+     * @var MasterSlaveConnection
+     */
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        parent::__construct();
+
+        $this->connection = $connection;
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $test = $this->connection->executeQuery('SELECT * FROM user');
+
+        dump($test->fetchAll(FetchMode::ASSOCIATIVE));
+    }
 
 }
